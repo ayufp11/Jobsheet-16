6649fd8b7ff1a65b530de9213ebf569f68802e66
luluk05 <luluk4696@gmail.com>
2022-10-25 07:19:23 +0700

    first commit

---
+++ app/src/main/java/com/example/ayufpjobsheet16/activity_dialog.kt
@@ -0,0 +1,18 @@
+package com.example.ayufpjobsheet16
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class ActivityDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_activity_dialog)
+    }
+}
